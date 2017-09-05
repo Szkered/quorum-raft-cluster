c22d54f85775866e4698584371c9f720f800f788
Ubuntu <azureuser@QuorumNx06.wd3akvwwparubno5ywgijuiejf.ix.internal.cloudapp.net>
2017-09-05 17:59:12 +0000

    update

--- raft-init.sh
+++ raft-init.sh
@@ -9,10 +9,10 @@ mkdir -p qdata/logs
 echo "[*] Configuring node"
 mkdir -p qdata/{keystore,geth}
 cp raft/static-nodes.json qdata
-# cp raft/keystore/$(ls raft/keystore) qdata/keystore/acckey
+cp raft/keystore/$(ls raft/keystore) qdata/keystore/acckey
 cp raft/geth/nodekey qdata/geth/nodekey
 geth --datadir qdata init genesis.json
 
-echo "[*] Creating default ethereum account"
-geth --datadir qdata --password passwords.txt account new
-killall geth
+# echo "[*] Creating default ethereum account"
+# geth --datadir qdata --password passwords.txt account new
+# killall geth
