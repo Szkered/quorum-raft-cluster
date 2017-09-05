1ab8184b9ea1fba53b73b2ad8da962fee2a34bf4
Ubuntu <azureuser@QuorumNx06.wd3akvwwparubno5ywgijuiejf.ix.internal.cloudapp.net>
2017-09-05 17:23:04 +0000

    fix setup script

--- raft-setup.sh
+++ raft-setup.sh
@@ -9,7 +9,7 @@ mkdir raft
 echo "[*] Generating geth node config"
 nohup geth --datadir raft 2>> raft/setup.log &
 sleep 3
-echo "[\"$(cat raft/setup.log | grep -oEi '(enode.*@)'):127.0.0.1:21000?discport=0&raftport=23000\"]" >> raft/static-nodes.json
+echo "[\"$(cat raft/setup.log | grep -oEi '(enode.*@)')127.0.0.1:21000?discport=0&raftport=23000\"]" >> raft/static-nodes.json
 
 echo "[*] Creating default ethereum account"
 geth --datadir raft --password passwords.txt account new
