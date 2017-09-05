45d8e0b4011cb84e8fffbbaf98f37edfca9e208d
Ubuntu <azureuser@QuorumNx06.wd3akvwwparubno5ywgijuiejf.ix.internal.cloudapp.net>
2017-09-05 17:41:45 +0000

    test

--- raft-setup.sh
+++ raft-setup.sh
@@ -11,8 +11,8 @@ nohup geth --datadir raft 2>> raft/setup.log &
 sleep 3
 echo "[\"$(cat raft/setup.log | grep -oEi '(enode.*@)')127.0.0.1:21000?discport=0&raftport=23000\"]" >> raft/static-nodes.json
 
-echo "[*] Creating default ethereum account"
-geth --datadir raft --password passwords.txt account new
+# echo "[*] Creating default ethereum account"
+# geth --datadir raft --password passwords.txt account new
 
 echo "[*] Stopping geth"
 killall geth
